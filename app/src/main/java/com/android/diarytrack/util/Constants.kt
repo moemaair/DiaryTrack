470f1cca9c20ad0e432e102efae99926aa232890
MohamedIbrahim <rageali12@gmail.com>
2023-02-06 20:01:20 +0300

    Add client id from Google cloud

--- app/src/main/java/com/android/diarytrack/util/Constants.kt
+++ app/src/main/java/com/android/diarytrack/util/Constants.kt
@@ -2,4 +2,5 @@ package com.android.diarytrack.util
 
 object Constants {
     const val APP_ID = "mydiarytrack-qdpgw"
+    const val CLIENT_ID = "https://307068373982-ebc0pdl155h4kecql1mrsriuriel6top.apps.googleusercontent.com/"
 }
