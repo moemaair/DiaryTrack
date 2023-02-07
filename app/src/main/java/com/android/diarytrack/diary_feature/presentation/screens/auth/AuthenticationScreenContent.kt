ec07385e9c9f15f54ca13b4275649a89e0d2bbec
MohamedIbrahim <rageali12@gmail.com>
2023-02-07 13:28:44 +0300

    Call Sign in with Google button

--- app/src/main/java/com/android/diarytrack/diary_feature/presentation/screens/auth/AuthenticationScreenContent.kt
+++ app/src/main/java/com/android/diarytrack/diary_feature/presentation/screens/auth/AuthenticationScreenContent.kt
@@ -1,10 +1,20 @@
 package com.android.diarytrack.diary_feature.presentation.screens.auth
 import androidx.compose.runtime.Composable
+import com.android.diarytrack.diary_feature.presentation.component.HomeGoogleButtonCustomized
 
 @Composable
 fun AuthenticationScreenContent(
     loadingState: Boolean,
     onButtonClick: () -> Unit
 ) {
+
+    //dummy text and icon
+
+    // sign in with google button
+
+    HomeGoogleButtonCustomized(
+        loadingState = loadingState,
+        onClick = onButtonClick
+    )
 
 }
