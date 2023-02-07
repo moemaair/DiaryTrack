3b3d1b3785eb9412ed3ffb9db79ddc4f9b3a66ff
MohamedIbrahim <rageali12@gmail.com>
2023-02-07 15:39:06 +0300

    Add NavGraph

---
+++ app/src/main/java/com/android/diarytrack/navigation/SetNavGraph.kt
@@ -0,0 +1,10 @@
+package com.android.diarytrack.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+
+@Composable
+fun SetNavGraph(startDestination: String, navController: NavHostController) {
+
+}
