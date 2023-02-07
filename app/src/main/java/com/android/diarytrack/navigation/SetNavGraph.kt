88ceb3e3d9c2536b85aa3c4d990518cd33fcbc59
MohamedIbrahim <rageali12@gmail.com>
2023-02-07 15:42:21 +0300

    Add NavHost Composable with argument set by NavGraph

--- app/src/main/java/com/android/diarytrack/navigation/SetNavGraph.kt
+++ app/src/main/java/com/android/diarytrack/navigation/SetNavGraph.kt
@@ -3,8 +3,14 @@ package com.android.diarytrack.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
 fun SetNavGraph(startDestination: String, navController: NavHostController) {
+    NavHost(
+        startDestination = startDestination,
+        navController = navController
+    ){
 
+    }
 }
