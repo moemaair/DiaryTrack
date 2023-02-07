d13e5242d2b390f8ed23cbf449793eac0c2bcdc4
MohamedIbrahim <rageali12@gmail.com>
2023-02-07 20:02:48 +0300

    Create OneTapSignInWithGoogle composable from stevdza-san's library

--- README.md
+++ README.md
@@ -46,4 +46,4 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ```
 
-[Back To The Top](#MiniTime)
+[Back To The Top](#Libraries used)
--- app/src/main/java/com/android/diarytrack/diary_feature/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/android/diarytrack/diary_feature/presentation/screens/auth/AuthenticationScreen.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.stevdzasan.onetap.OneTapSignInWithGoogle
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -18,6 +19,19 @@ fun AuthenticationScreen(
                 loadingState = loadingState,
                 onButtonClick = onButtonClick
             )
+        }
+    )
+    //stevdza-san
+    //OneTapCompose Library
+
+    OneTapSignInWithGoogle(
+        state = ,
+        clientId = ,
+        onTokenIdReceived = {tokenId->
+
+        },
+        onDialogDismissed = {message->
+
         }
     )
 }
