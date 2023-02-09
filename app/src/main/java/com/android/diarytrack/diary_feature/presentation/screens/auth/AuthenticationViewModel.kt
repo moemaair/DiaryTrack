8ebf6194dd7b8644df47c3ff5ebcfb82ad41ff9c
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 12:30:27 +0300

    Add a loadingState set to default value false and can be modified within the viewmodel

--- app/src/main/java/com/android/diarytrack/diary_feature/presentation/screens/auth/AuthenticationViewModel.kt
+++ app/src/main/java/com/android/diarytrack/diary_feature/presentation/screens/auth/AuthenticationViewModel.kt
@@ -1,5 +1,7 @@
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 class AuthenticationViewModel : ViewModel(){
-
+    var loadingState = mutableStateOf(false)
+        private set
 }
