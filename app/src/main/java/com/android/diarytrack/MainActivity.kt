d146df04cbad21d77575334302fcc011940b186f
MohamedIbrahim <rageali12@gmail.com>
2023-02-09 13:38:40 +0300

    Add a logic that will check if the user is logged in and will take them to Home Screen automatically

--- app/src/main/java/com/android/diarytrack/MainActivity.kt
+++ app/src/main/java/com/android/diarytrack/MainActivity.kt
@@ -5,19 +5,16 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
-import com.android.diarytrack.diary_feature.presentation.component.HomeGoogleButtonCustomized
-import com.android.diarytrack.diary_feature.presentation.screens.auth.AuthenticationScreen
 import com.android.diarytrack.navigation.Screen
 import com.android.diarytrack.navigation.SetNavGraph
 import com.android.diarytrack.ui.theme.DiaryTrackTheme
-
+import com.android.diarytrack.util.Constants.APP_ID
+import io.realm.kotlin.mongodb.App
 
 
 class MainActivity : ComponentActivity() {
@@ -26,15 +23,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-
         setContent {
             DiaryTrackTheme {
                     val navController = rememberNavController()
-                    SetNavGraph(startDestination = Screen.AuthenticationScreen.route,
+                    SetNavGraph(startDestination = getStartDestination(),
                         navController = navController )
             }
         }
     }
+}
+
+private fun getStartDestination(): String{
+    val user = App.create(APP_ID).currentUser
+    return if(user != null && user.loggedIn)
+        Screen.Home.route else
+            Screen.AuthenticationScreen.route
+
 }
 
 
