ca029a81642bc7353d80e6e64c0c58ef3697a00f
moemaair <rageali12@gmail.com>
2023-02-20 20:51:09 +0300

    Added an enum class for every logic of card header

---
+++ app/src/main/java/com/android/diarytrack/diary_feature/model/Mood.kt
@@ -0,0 +1,11 @@
+package com.android.diarytrack.diary_feature.model
+
+import androidx.compose.ui.graphics.Color
+
+enum class Mood (
+    val icon: Int,
+    val containerColor: Color, // rep background of the card header
+    val contentColor: Color
+
+    ){
+}
