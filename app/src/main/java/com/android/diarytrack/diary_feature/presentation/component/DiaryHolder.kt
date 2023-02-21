23b1c16847f81e869640b49b128fe1f33fb676c0
moemaair <rageali12@gmail.com>
2023-02-21 22:14:52 +0300

    added a composable for holding diary item

---
+++ app/src/main/java/com/android/diarytrack/diary_feature/presentation/component/DiaryHolder.kt
@@ -0,0 +1,9 @@
+package com.android.diarytrack.diary_feature.presentation.component
+
+import androidx.compose.runtime.Composable
+import java.time.LocalDate
+
+@Composable
+fun DiaryHolder(local: LocalDate) {
+
+}
